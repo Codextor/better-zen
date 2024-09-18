0dd67952b1930ac533cf0bdbad3ae2b1f1c63668
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-09-18 23:28:28 +0530

    Update zen version tested

--- better-zen/user.js
+++ better-zen/user.js
@@ -1,5 +1,5 @@
 // Better Zen Profile User Preferences
-// Tested with Zen version 1.0.1-a.1
+// Tested with Zen version 1.0.1-a.2
 
 // If you make changes to your preferences from about:config in the browser,
 // the changes will be overwritten by the below preferences on application restart.
