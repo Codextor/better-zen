b073a0c164328353a38daf0375eae016aa989f01
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-11-01 07:11:18 +0530

    Changes for version 1.0.1-a.15

--- better-zen/user.js
+++ better-zen/user.js
@@ -1,5 +1,5 @@
 // Better Zen Profile User Preferences
-// Tested with Zen version 1.0.1-a.14
+// Tested with Zen version 1.0.1-a.15
 
 // If you make changes to your preferences from about:config in the browser,
 // the changes will be overwritten by the below preferences on application restart.
@@ -8,7 +8,10 @@
 
 user_pref("browser.contentblocking.category", "strict");
 user_pref("browser.profiles.enabled", true);
+user_pref("browser.sessionstore.interval", 60000);
 user_pref("browser.tabs.tabmanager.enabled", false);
+
+user_pref("captivedetect.canonicalURL", "");
 
 user_pref("content.notify.interval", 100000);
 
@@ -19,17 +22,35 @@ user_pref("dom.security.https_first", true);
 
 user_pref("editor.truncate_user_pastes", false);
 
+user_pref("gfx.canvas.accelerated.cache-items", 4096);
+user_pref("gfx.canvas.accelerated.cache-size", 512);
+user_pref("gfx.content.skia-font-cache-size", 20);
+
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.memory_cache_max_size", 65536);
 user_pref("media.peerconnection.ice.default_address_only", true);
 user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", true);
 
 user_pref("network.auth.subresource-http-auth-allow", 1);
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.dns.disablePrefetchFromHTTPS", true);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 user_pref("network.predictor.enabled", false);
 user_pref("network.prefetch-next", false);
+user_pref("network.ssl_tokens_cache_capacity", 10240);
 
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("permissions.manager.defaultsUrl", "");
+
+user_pref("security.OCSP.enabled", 0);
 user_pref("security.mixed_content.block_display_content", true);
+user_pref("security.pki.crlite_mode", 2);
+user_pref("security.remote_settings.crlite_filters.enabled", true);
 
 user_pref("signon.formlessCapture.enabled", false);
 user_pref("signon.privateBrowsingCapture.enabled", false);
+
+user_pref("webchannel.allowObject.urlWhitelist", "");
